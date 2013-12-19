ef13b840c4a38598c70742d1947996994b108742
Steven Peters <scpeters@osrfoundation.org>
2013-12-18 21:58:50 -0800

    Gazebo: point HEAD to gazebo_1.9 branch
    On this branch, revert to 1.9.2 and use gazebo_1.9 as HEAD

--- gazebo.rb
+++ gazebo.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Gazebo < Formula
   homepage 'http://gazebosim.org'
-  url 'http://gazebosim.org/assets/distributions/gazebo-current-2.1.0.tar.bz2'
-  sha1 '0db594c400ccb619ff1a3db6b6d02333d6d0e3a7'
-  head 'https://bitbucket.org/osrf/gazebo', :branch => 'default', :using => :hg
+  url 'http://gazebosim.org/assets/distributions/gazebo-1.9.2.tar.bz2'
+  sha1 '6eecb681c289dbb4b37e922d7239e8c96db43b83'
+  head 'https://bitbucket.org/osrf/gazebo', :branch => 'gazebo_1.9', :using => :hg
 
   depends_on 'boost'
   depends_on 'cmake'  => :build
